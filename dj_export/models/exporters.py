026d286da19e76c4b055acd52e666c9343023a18
rockvns <chandrabhanvns@gmail.com>
2024-08-20 23:56:53 +0530

    BaseExport class for managing common actions

--- dj_export/models/exporters.py
+++ dj_export/models/exporters.py
@@ -1,26 +1,33 @@
 from django.db.models.query import QuerySet
 import polars as pl
+from openpyxl import Workbook
 
-class ExcelExport:
+class BaseExportClass:
     @classmethod
-    def export_config(cls, queryset: QuerySet, filename: str):
+    def apply_config(cls, queryset: QuerySet, filename: str):
         # Your configuration logic here
+        cls.manage_seetname()
+
+    @classmethod
+    def manage_seetname(cls, wb: Workbook, sheet_config:dict):
+        # manager sheet related actions here form config part
         pass
 
+
+class ExcelExport(BaseExportClass):
     @classmethod
     def export(cls, queryset: QuerySet, filename: str):
         data = list(queryset.values())
         df = pl.DataFrame(data)
         df.write_excel(filename, fmt='xlsx')
+        cls.apply_config()
 
-class CSVExport:
-    @classmethod
-    def export_config(cls, queryset: QuerySet, filename: str):
-        # Your configuration logic here
-        pass
 
+class CSVExport(BaseExportClass):
+    
     @classmethod
     def export(cls, queryset: QuerySet, filename: str):
         data = list(queryset.values())
         df = pl.DataFrame(data)
         df.write_csv(filename)
+        cls.apply_config()
